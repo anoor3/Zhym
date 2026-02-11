b5525cf151a7e50cb6992332e5366423d6af8b90
Abdullah Noor <abdullahnoor.edu1@gmail.com>
2026-02-10 19:15:37 -0800

    Made Zhym Structure for ios app

---
+++ Zhym/Zhym/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zhym
+//
+//  Created by Abdullah Noor on 2/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zhym/Zhym/ZhymApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZhymApp.swift
+//  Zhym
+//
+//  Created by Abdullah Noor on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct ZhymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
